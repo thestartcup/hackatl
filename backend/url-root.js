8c123d35c291488f5068f9d364433a2f7342a2ca
Brent Bessemer <bmb0610@gmail.com>
2017-11-11 18:07:35 -0500

    Finished(?) url-root

--- backend/url-root.js
+++ backend/url-root.js
@@ -6,17 +6,25 @@
   // where base is one of [user, org]
   // scheme is one of [tag, search, id]
   // name only relevant in tag and id schemes
-  var parseUrlRoot = (base, scheme, name, response) => {
+  var serveUrlRoot = (base, scheme, name, response) => {
     var db = database.load(base)
-    if (!db) { return; }
-    if (scheme == 'tag') {
-      db.serveTag(name, response);
-    } else if (scheme == 'search') {
-      db.serveSearch(url.query, response)
-    } else if (scheme == 'id') {
-      db.serveId(name, response)
-    } else {
-      db.serveError()
+    if (db) {
+      if (scheme == 'tag') {
+        db.serveTag(name, response);
+      } else if (scheme == 'search') {
+        db.serveSearch(url.query, response)
+      } else if (scheme == 'id') {
+        db.serveId(name, response)
+      } else {
+        db.serveError()
+      }
+    }
+  }
+
+  var putUrlRoot = (base, item, response) => {
+    var db = database.load(base)
+    if (db) {
+      db.putItem(item)
     }
   }
 })();
